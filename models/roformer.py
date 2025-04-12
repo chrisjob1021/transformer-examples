6b2a2254f77701a37a0ad31138117423a28b0ce9
Chris O'Brien <chris.obrien.1021@gmail.com>
2025-04-12 16:56:53 -0700

    wip

--- models/roformer.py
+++ models/roformer.py
@@ -1,7 +1,25 @@
+import attention
+
 class RoFormerEncoderLayer(nn.Module):
     def __init__(self, config: Config):
         super().__init__()
         self.config = config
 
-        self.self_attn = MultiHeadLatentAttention(config, rope=True)
+        self.self_attn = attention.MultiHeadAttention(config, rope=True)
+        
+        # https://pytorch.org/docs/stable/generated/torch.nn.LayerNorm.html#torch.nn.LayerNorm
+        # Applies Layer Normalization over a mini-batch of inputs.
+        # This layer implements the operation as described in the paper Layer Normalization
+        # (Ba et al., 2016: https://arxiv.org/abs/1607.06450)
         self.ln1 = nn.LayerNorm(config.d_model)
+        self.dropout1 = nn.Dropout(config.dropout)
+
+        # Position-wise feed-forward
+        # Using nn.sequential feels like cheating :)
+        self.ffn = nn.Sequential(
+            nn.Linear(config.d_model, config.ffn_dim),
+            nn.ReLU(),
+            nn.Linear(config.ffn_dim, config.d_model),
+        )
+        self.ln2 = nn.LayerNorm(config.d_model)
+        self.dropout2 = nn.Dropout(config.dropout)
--- utils/config.py
+++ utils/config.py
@@ -1,14 +1,15 @@
 class Config:
-    def __init__(self, d_model=256, num_heads=4, max_len=1024, dropout=0.1, rope=False):
+    def __init__(self, d_model=256, num_heads=4, max_len=1024, dropout=0.1, rope=False, ffn_dim=256):
         self.d_model = d_model
         self.num_heads = num_heads
         self.per_head_dim = d_model // num_heads
         self.max_len = max_len
         self.dropout = dropout
         self.rope = rope
+        self.ffn_dim = ffn_dim
 
 class MLAConfig(Config):
-    def __init__(self, d_model=256, num_heads=4, max_len=1024, dropout=0.1, rope=False):
-        super().__init__(d_model, num_heads, max_len, dropout, rope)
+    def __init__(self, d_model=256, num_heads=4, max_len=1024, dropout=0.1, rope=False, ffn_dim=256):
+        super().__init__(d_model, num_heads, max_len, dropout, rope, ffn_dim)
         self.d_model_prime_compressed = d_model*num_heads // 16
         self.d_model_compressed = d_model*num_heads // 16
