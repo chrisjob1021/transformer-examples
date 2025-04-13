815e45a9b4547965049ac73327bbb0e30d5f3fa2
Chris O'Brien <chris.obrien.1021@gmail.com>
2025-04-12 19:52:09 -0700

    getting ready to train

--- utils/config.py
+++ utils/config.py
@@ -8,6 +8,13 @@ class Config:
         self.rope = rope
         self.ffn_dim = ffn_dim
 
+class TrainingConfig:
+    def __init__(self, steps=100, batch_size=32, learning_rate=1e-4, weight_decay=0.01):
+        self.steps = steps
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
+
 class MLAConfig(Config):
     def __init__(self, d_model=256, num_heads=4, max_len=1024, dropout=0.1, rope=False, ffn_dim=256):
         super().__init__(d_model, num_heads, max_len, dropout, rope, ffn_dim)
