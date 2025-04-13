52e75b47c13e19fb13ec98598f4c561052547d79
Chris O'Brien <chris.obrien.1021@gmail.com>
2025-04-12 17:29:21 -0700

    encoder layer

--- models/roformer.py
+++ models/roformer.py
@@ -11,8 +11,8 @@ class RoFormerEncoderLayer(nn.Module):
         # Applies Layer Normalization over a mini-batch of inputs.
         # This layer implements the operation as described in the paper Layer Normalization
         # (Ba et al., 2016: https://arxiv.org/abs/1607.06450)
-        self.ln1 = nn.LayerNorm(config.d_model)
         self.dropout1 = nn.Dropout(config.dropout)
+        self.ln1 = nn.LayerNorm(config.d_model)
 
         # Position-wise feed-forward
         # Using nn.sequential feels like cheating :)
@@ -21,5 +21,15 @@ class RoFormerEncoderLayer(nn.Module):
             nn.ReLU(),
             nn.Linear(config.ffn_dim, config.d_model),
         )
-        self.ln2 = nn.LayerNorm(config.d_model)
         self.dropout2 = nn.Dropout(config.dropout)
+        self.ln2 = nn.LayerNorm(config.d_model)
+
+    def forward(self, x):
+        attn_output = self.self_attn(x)
+        x = x + self.dropout1(attn_output)
+        x = self.ln1(x)
+
+        ffn_output = self.ffn(x)
+        x = x + self.dropout2(ffn_output)
+        x = self.ln2(x)
+        return x
