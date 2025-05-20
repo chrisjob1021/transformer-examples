011d9e56d7d608ee63d82b24a8d8115d8b9da3fd
Chris O'Brien <chris@chrisobrien.ai>
2025-05-20 15:54:16 +0000

    baseline training settings

--- models/roformer/roformer_train.py
+++ models/roformer/roformer_train.py
@@ -88,10 +88,10 @@ args = TrainingArguments(
     # warmup_steps=2_000,
     # Specify AdamW optimizer
     optim="adamw_torch",
-    weight_decay=0.1,
-    max_grad_norm=1.0,
+    weight_decay=0.01,
+    max_grad_norm=0.5,
 
-    max_steps=60_000,
+    max_steps=20_000,
     per_device_train_batch_size=4,
     gradient_accumulation_steps=16, # Accumulate gradients over N steps
     #With gradient accumulation (gradient_accumulation_steps=8):
